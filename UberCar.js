7b04e91fee9c974521a7fa9996c3b08bb1cffc88
Jamileth987 <raquegar021@gmail.com>
2025-02-17 19:23:26 -0600

    Add files via upload

---
+++ UberCar.js
@@ -0,0 +1,13 @@
+// Este programa pide dos números al usuario y los suma
+
+// Solicitar el primer número
+let num1 = parseFloat(prompt("Introduce el primer número:"));
+
+// Solicitar el segundo número
+let num2 = parseFloat(prompt("Introduce el segundo número:"));
+
+// Realizar la suma
+let suma = num1 + num2;
+
+// Mostrar el resultado en un mensaje emergente
+alert("La suma de " + num1 + " y " + num2 + " es: " + suma);
