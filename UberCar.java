4d37ff3ce4fbc3356dec05ae3a887c781e817c43
Jamileth987 <raquegar021@gmail.com>
2025-02-17 19:22:50 -0600

    Add files via upload

---
+++ UberCar.java
@@ -0,0 +1,9 @@
+// Definición de la clase principal
+public class HolaMundo {
+
+    // Método principal, punto de entrada del programa
+    public static void main(String[] args) {
+        // Imprime "Hola Mundo" en la consola
+        System.out.println("Hola Mundo");
+    }
+}
